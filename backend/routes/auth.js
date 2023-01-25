48b8c9dc305b9d7e8f73b2eb732b269d1e2bab5c
ashish <ashish.ksingh8765@gmail.com>
2023-01-25 13:51:12 +0530

    Initial commit

---
+++ backend/routes/auth.js
@@ -0,0 +1,129 @@
+const express = require("express");
+const User = require("../models/User");
+var jwt = require("jsonwebtoken");
+const router = express.Router();
+const { body, validationResult } = require("express-validator");
+const bcrypt = require("bcryptjs");
+var fetchuser = require("../middleware/fetchuser");
+
+const JWT_SECRET = "@shissmartboy6";
+
+//Route 1: Create a User using: POST "/api/auth/createuser" . No Login Required
+router.post(
+  "/createuser",
+  [
+    body("name", "Enter a valid name").isLength({ min: 2 }),
+    body("email", "Enter a valid email").isEmail(),
+    body("password", "Password must be atleast 5 characters").isLength({
+      min: 5,
+    }),
+  ],
+  async (req, res) => {
+    let success = false;
+    // if there are errors return bad request and the errors
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ success, errors: errors.array() });
+    }
+    try {
+      // Check whether the user with this email exists already
+      let user = await User.findOne({ email: req.body.email });
+      if (user) {
+        return res
+          .status(400)
+          .json({
+            success,
+            error: "Sorry a user with this email already exists",
+          });
+      }
+
+      // Encrypting Password
+      const salt = await bcrypt.genSalt(10);
+      securePassword = await bcrypt.hash(req.body.password, salt);
+
+      // Create a new user
+      user = await User.create({
+        name: req.body.name,
+        email: req.body.email,
+        password: securePassword,
+      });
+      const data = {
+        user: {
+          id: user.id,
+        },
+      };
+      const authToken = jwt.sign(data, JWT_SECRET);
+      success = true;
+      res.json({ success, authToken });
+    } catch (error) {
+      console.error(error.message);
+      res.status(500).send("Internal Server error");
+    }
+  }
+);
+
+//Route 2: Authenticate a User using: POST "/api/auth/login" . No Login Required
+
+router.post(
+  "/login",
+  [
+    body("email", "Enter a valid email").isEmail(),
+    body("password", "Password cannot not be blank").exists(),
+  ],
+  async (req, res) => {
+    let success = false;
+    // if there are errors return bad request and the errors
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { email, password } = req.body;
+    try {
+      let user = await User.findOne({ email });
+      if (!user) {
+        success = false;
+        return res
+          .status(400)
+          .json({ error: "Please try to login with correct credentials" });
+      }
+
+      const passwordCompare = await bcrypt.compare(password, user.password);
+      if (!passwordCompare) {
+        success = false;
+        return res
+          .status(400)
+          .json({
+            success,
+            error: "Please try to login with correct credentials",
+          });
+      }
+
+      const data = {
+        user: {
+          id: user.id,
+        },
+      };
+      const authToken = jwt.sign(data, JWT_SECRET);
+      success = true;
+      res.json({ success, authToken });
+    } catch (error) {
+      console.error(error.message);
+      res.status(500).send("Internal Server error");
+    }
+  }
+);
+
+//Route 3: Get Logged in user Details using: POST "/api/auth/getuser". Login Required
+router.post("/getuser", fetchuser, async (req, res) => {
+  try {
+    const userId = req.user.id;
+    const user = await User.findById(userId).select("-password");
+    res.send(user);
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send("Internal Server error");
+  }
+});
+
+module.exports = router;
---
+++ src/components/About.js
@@ -0,0 +1,9 @@
+// import React, { useContext, useEffect } from "react";
+import React from "react";
+
+const About = () => {
+  
+  return (<div>This is About page. Get all Details here!</div>);
+};
+
+export default About;
